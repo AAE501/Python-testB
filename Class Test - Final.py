aee895c1c48a3785d16c2c7cd4fe54d67397b404
AAE501 <t0461784@my.ntu.ac.uk>
2025-11-04 11:58:54 +0000

    Update Class Test - Final.py
    PS C:\Users\T0461784> C:/ProgramData/Anaconda3/Scripts/activate
    PS C:\Users\T0461784> conda activate base
    conda : The term 'conda' is not recognized as the name of a cmdlet, function, script file, or operable program. Check the spelling of the name, or if a path was included, verify that the path is correct and try again.
    At line:1 char:1
    + conda activate base
    + ~~~~~
        + CategoryInfo          : ObjectNotFound: (conda:String) [], CommandNotFoundException
        + FullyQualifiedErrorId : CommandNotFoundException
    
    PS C:\Users\T0461784> & C:/ProgramData/Anaconda3/python.exe "c:/Users/T0461784/task 1.py"
      File "c:\Users\T0461784\task 1.py", line 6
        print(IQ is normal intelligence)
                           ^
    SyntaxError: invalid syntax
    PS C:\Users\T0461784> & C:/ProgramData/Anaconda3/python.exe "c:/Users/T0461784/task 1.py"
      File "c:\Users\T0461784\task 1.py", line 6
        print(IQ is normal intelligence)
                           ^
    SyntaxError: invalid syntax
    PS C:\Users\T0461784> & C:/ProgramData/Anaconda3/python.exe "c:/Users/T0461784/task 1.py"
      File "c:\Users\T0461784\task 1.py", line 6
        print(IQ is normal intelligence)
                           ^
    SyntaxError: invalid syntax
    PS C:\Users\T0461784> & C:/ProgramData/Anaconda3/python.exe "c:/Users/T0461784/task 1.py"
      File "c:\Users\T0461784\task 1.py", line 6
        print(IQ is normal intelligence)
                           ^
    SyntaxError: invalid syntax
    PS C:\Users\T0461784> & C:/ProgramData/Anaconda3/python.exe "c:/Users/T0461784/task 1.py"
      File "c:\Users\T0461784\task 1.py", line 7
        if IQ>180
                 ^
    SyntaxError: invalid syntax
    PS C:\Users\T0461784> & C:/ProgramData/Anaconda3/python.exe "c:/Users/T0461784/task 1.py"
    The IQ = 80.0
    This indicates normal intelligence
    PS C:\Users\T0461784>

--- Class Test - Final.py
+++ Class Test - Final.py
@@ -1,13 +1,18 @@
-IQTA = 500
-IQTB = 700
-IQTC = 15
+user_ch = 'Y'
 
-IQ = (IQTA + IQTB) / IQTC
-print("The IQ =", IQ)
+while user_ch == 'Y' or user_ch == 'y':
+    IQTA = int(input("Enter test A results: "))
+    IQTB = int(input("Enter test B results: "))
+    IQTC = int(input("Enter test C results: "))
 
-if IQ > 180:
-    print("This indicates Genius")
-elif IQ > 120:
-    print("This indicates Highly intelligent")
-else:
-    print("This indicates normal intelligence")
+    IQ = (IQTA + IQTB) / IQTC
+    print("The IQ =", IQ)
+
+    if IQ > 180:
+        print("This indicates Genius")
+    elif IQ > 120:
+        print("This indicates Highly intelligent")
+    else:
+        print("This indicates normal intelligence")
+
+    user_ch = input("Another IQ to calculate? (y/n): ")
