a44be35a1facaae21f6628a39559dca9d920b93e
AAE501 <t0461784@my.ntu.ac.uk>
2025-11-04 12:00:42 +0000

    Update Class Test - Final.py
    PS C:\Users\T0461784> C:/ProgramData/Anaconda3/Scripts/activate
    PS C:\Users\T0461784> conda activate base
    conda : The term 'conda' is not recognized as the name of a cmdlet, function, script file, or operable program. Check the spelling of the name, or if a path was included, verify that the path is correct and try again.
    At line:1 char:1
    + conda activate base
    + ~~~~~
        + CategoryInfo          : ObjectNotFound: (conda:String) [], CommandNotFoundException
        + FullyQualifiedErrorId : CommandNotFoundException
    
    PS C:\Users\T0461784> & C:/ProgramData/Anaconda3/python.exe "c:/Users/T0461784/task 1.py"
      File "c:\Users\T0461784\task 1.py", line 6
        print(IQ is normal intelligence)
                           ^
    SyntaxError: invalid syntax
    PS C:\Users\T0461784> & C:/ProgramData/Anaconda3/python.exe "c:/Users/T0461784/task 1.py"
      File "c:\Users\T0461784\task 1.py", line 6
        print(IQ is normal intelligence)
                           ^
    SyntaxError: invalid syntax
    PS C:\Users\T0461784> & C:/ProgramData/Anaconda3/python.exe "c:/Users/T0461784/task 1.py"
      File "c:\Users\T0461784\task 1.py", line 6
        print(IQ is normal intelligence)
                           ^
    SyntaxError: invalid syntax
    PS C:\Users\T0461784> & C:/ProgramData/Anaconda3/python.exe "c:/Users/T0461784/task 1.py"
      File "c:\Users\T0461784\task 1.py", line 6
        print(IQ is normal intelligence)
                           ^
    SyntaxError: invalid syntax
    PS C:\Users\T0461784> & C:/ProgramData/Anaconda3/python.exe "c:/Users/T0461784/task 1.py"
      File "c:\Users\T0461784\task 1.py", line 7
        if IQ>180
                 ^
    SyntaxError: invalid syntax
    PS C:\Users\T0461784> & C:/ProgramData/Anaconda3/python.exe "c:/Users/T0461784/task 1.py"
    The IQ = 80.0
    This indicates normal intelligence
    PS C:\Users\T0461784> & C:/ProgramData/Anaconda3/python.exe "c:/Users/T0461784/OneDrive - Nottingham Trent University/Documents/GitHub/Python-testB/Class Test - Final.py"
    Enter test A results:

--- Class Test - Final.py
+++ Class Test - Final.py
@@ -1,18 +1,38 @@
 user_ch = 'Y'
+IQ_n = 0
+IQ_sum = 0
 
-while user_ch == 'Y' or user_ch == 'y':
-    IQTA = int(input("Enter test A results: "))
-    IQTB = int(input("Enter test B results: "))
-    IQTC = int(input("Enter test C results: "))
+while user_ch.lower() == 'y':
+    try:
+        IQTA = int(input("Enter test A results: "))
+        IQTB = int(input("Enter test B results: "))
+        IQTC = int(input("Enter test C results: "))
 
-    IQ = (IQTA + IQTB) / IQTC
-    print("The IQ =", IQ)
+        if IQTC == 0:
+            print("Error: Test C result cannot be zero.")
+            continue
 
-    if IQ > 180:
-        print("This indicates Genius")
-    elif IQ > 120:
-        print("This indicates Highly intelligent")
-    else:
-        print("This indicates normal intelligence")
+        IQ = (IQTA + IQTB) / IQTC
+        print(f"The IQ = {IQ:.2f}")
+
+        IQ_n += 1
+        IQ_sum += IQ
+
+        if IQ > 180:
+            print("This indicates Genius")
+        elif IQ > 120:
+            print("This indicates Highly intelligent")
+        else:
+            print("This indicates normal intelligence")
+
+    except ValueError:
+        print("Please enter valid numeric values.")
+        continue
 
     user_ch = input("Another IQ to calculate? (y/n): ")
+
+if IQ_n > 0:
+    IQ_avg = IQ_sum / IQ_n
+    print(f"A total of {IQ_n} IQs were calculated with an average of {IQ_avg:.2f}")
+else:
+    print("No IQs were calculated.")
